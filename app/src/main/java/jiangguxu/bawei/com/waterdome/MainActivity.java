84d698494e13df09f7c053171093806fae92a6de
JiangGuXu <1324837706@qq.com>
2018-10-07 18:48:43 +0800

    第一次提交

---
+++ app/src/main/java/jiangguxu/bawei/com/waterdome/MainActivity.java
@@ -0,0 +1,29 @@
+package jiangguxu.bawei.com.waterdome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import jiangguxu.bawei.com.waterdome.widget.WaterView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView img;
+    private WaterView waterView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.img_icon);
+        final RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) img.getLayoutParams();
+        waterView = findViewById(R.id.water_view);
+        waterView.setAnimationListener(new WaterView.AnimationListener() {
+            @Override
+            public void animation(float y) {
+                layoutParams.setMargins(0,0,0, (int) y);
+                img.setLayoutParams(layoutParams);
+            }
+        });
+    }
+}
